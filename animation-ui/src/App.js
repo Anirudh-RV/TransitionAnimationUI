03f33f439858c5b7227a0da006135e245cff2207
anirudhrv <anirudhrv@Raghavans-MacBook-Air.local>
2021-10-23 14:13:23 +0530

    Image overlay slider completed

--- animation-ui/src/App.js
+++ animation-ui/src/App.js
@@ -1,22 +1,80 @@
+import React, { Component } from "react";
+import RangeStepInput from "./RangeStepInput";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div class="w-full h-screen bg-gray-800 flex justify-center items-center">
-      <div class="bg-gray-700 w-6/12 h-6/12 relative z-40">
-        <div class="absolute inset-0 flex justify-center items-center z-10">
-          <img src="Task1RAW_Image.jpeg" alt="Raw Image" class="bg-gray-900" />
-        </div>
-        <div class="absolute inset-0 flex justify-center items-center z-50">
-          <img
-            src="Task1Processed_Image.jpeg"
-            alt="Processed Image"
-            class="bg-gray-900 mix-blend-lighten ImageClip"
-          />
+const forceNumber = function (n) {
+  n = Number(n);
+  if (isNaN(n) || typeof n === "undefined") {
+    n = 0;
+  }
+  return n;
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clipPercentage: 50,
+    };
+  }
+  IncrementClip = () => {
+    console.log("Incrementing Clip");
+    this.setState({
+      clipPercentage: this.state.clipPercentage + 10,
+    });
+  };
+
+  DecrementClip = () => {
+    console.log("Decrementing Clip");
+    this.setState({
+      clipPercentage: this.state.clipPercentage - 10,
+    });
+  };
+
+  onChange = (e) => {
+    const newVal = forceNumber(e.target.value);
+    this.setState({ clipPercentage: newVal });
+  };
+
+  render() {
+    return (
+      <div class="w-full h-screen bg-gray-800 flex justify-center items-center">
+        <div class="bg-gray-700 w-6/12 h-6/12 relative">
+          <div class="absolute inset-0 flex justify-center items-center">
+            <img
+              onClick={this.DecrementClip}
+              src="Task1RAW_Image.jpeg"
+              alt="Raw"
+              class="bg-gray-900"
+              ref={(c) => (this.leftImage = c)}
+            />
+          </div>
+          <div class="absolute inset-0 flex justify-center items-center">
+            <img
+              onClick={this.IncrementClip}
+              src="Task1Processed_Image.jpeg"
+              alt="Processed"
+              class="bg-gray-900 mix-blend-lighten"
+              ref={(c) => (this.rightImage = c)}
+              style={{
+                clipPath: "inset(0 0 0 " + this.state.clipPercentage + "%)",
+              }}
+            />
+          </div>
+          <div class="absolute inset-0 flex justify-center items-center">
+            <RangeStepInput
+              min={0}
+              max={100}
+              value={this.state.clipPercentage}
+              step={1}
+              onChange={this.onChange.bind(this)}
+              class="absolute inset-0 flex justify-center items-center"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
---
+++ animation-ui/src/RangeStepInput.js
@@ -0,0 +1,164 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./App.css";
+
+const forceNumber = function (n) {
+  n = Number(n);
+  if (isNaN(n) || typeof n === "undefined") {
+    n = 0;
+  }
+  return n;
+};
+/**
+ * This is an <input type=range> that steps up and down on click
+ * instead of jumping immediately to the new value.
+ *
+ * Based on zcorpan's solution here:
+ *   https://stackoverflow.com/a/51988783/173630
+ */
+export default class RangeStepInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMouseDown: false,
+      isDragging: false,
+    };
+    this.onInput = this.onInput.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+
+    this.domRef = React.createRef();
+  }
+
+  onMouseDown() {
+    this.setState({ isMouseDown: true });
+
+    if (this.props.hold) {
+      if (this.holdLoop) {
+        clearInterval(this.holdLoop);
+      }
+
+      let oldVal = this.props.value;
+
+      const self = this;
+      setTimeout(function () {
+        if (self.holdLoop) {
+          clearInterval(self.holdLoop);
+        }
+        self.holdLoop = self.makeHoldLoop(oldVal);
+        // Add some initial delay on the click-hold functionality.
+      }, 250);
+    }
+  }
+  onMouseUp() {
+    this.setState({
+      isMouseDown: false,
+      isDragging: false,
+    });
+
+    if (this.holdLoop) {
+      clearInterval(this.holdLoop);
+    }
+  }
+  onMouseMove() {
+    if (this.state.isMouseDown) {
+      this.setState({ isDragging: true });
+    }
+  }
+  onInput(e) {
+    const step = this.props.step;
+    const newVal = forceNumber(e.target.value);
+    const oldVal = this.props.value;
+
+    if (
+      // Disable the oninput filter with the user is dragging
+      // the slider's knob.
+      !(this.state.isMouseDown && this.state.isDragging) &&
+      oldVal
+    ) {
+      e.target.value = newVal > oldVal ? oldVal + step : oldVal - step;
+    }
+  }
+  makeHoldLoop(oldVal) {
+    const self = this;
+
+    return setInterval(function () {
+      if (!self.state.isMouseDown || self.state.isDragging) {
+        // The user isn't holding the cursor anymore, or the cursor
+        // is being dragged. Clean up and cancel.
+        if (self.holdLoop) {
+          clearInterval(self.holdLoop);
+        }
+        return false;
+      }
+
+      const input = self.domRef.current;
+      let newVal = self.props.value;
+
+      if (oldVal > newVal && newVal - self.props.step >= self.props.min) {
+        newVal -= self.props.step;
+      } else if (
+        oldVal < newVal &&
+        newVal + self.props.step <= self.props.max
+      ) {
+        newVal += self.props.step;
+      }
+
+      if (oldVal === newVal) {
+        return false;
+      }
+
+      // Directly setting input.value will cause the new value
+      // to not be recognized, because of React.
+      // https://stackoverflow.com/a/46012210/173630
+      const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+        window.HTMLInputElement.prototype,
+        "value"
+      ).set;
+      nativeInputValueSetter.call(input, newVal);
+
+      // Trigger an onChange event.
+      const e = new Event("change", { bubbles: true });
+
+      return input.dispatchEvent(e);
+    }, 100);
+  }
+
+  render() {
+    return (
+      <input
+        type="range"
+        ref={this.domRef}
+        style={{ background: "inherit" }}
+        min={this.props.min}
+        max={this.props.max}
+        step={this.props.step}
+        value={this.props.value}
+        disabled={this.props.disabled}
+        onChange={this.props.onChange}
+        onMouseDown={this.onMouseDown}
+        onMouseUp={this.onMouseUp}
+        onMouseMove={this.onMouseMove}
+        onClick={this.onClick}
+        onInput={this.onInput}
+      />
+    );
+  }
+}
+
+RangeStepInput.propTypes = {
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  step: PropTypes.number.isRequired,
+  min: PropTypes.number,
+  max: PropTypes.number,
+  disabled: PropTypes.bool,
+  // Determines whether the slider changes value when the cursor is
+  // held on it.
+  hold: PropTypes.bool,
+};
+
+RangeStepInput.defaultProps = {
+  hold: true,
+};
